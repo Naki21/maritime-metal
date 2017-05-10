e456870a037912cd77062811e4edc05d79b14ff2
akradin <alex.kradin@gmail.com>
2017-05-10 13:45:20 -0400

    Added carousel functionality to display items sold
    Found code that generates a carousel and altered it so it only
    contains 11 panels which show the items MMCo sells.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,24 +1,26 @@
 'use strict';
-const setAPIOrigin = require('../../lib/set-api-origin')
-const config = require('./config')
-const clickhandlers = require('./clickhandlers')
-import ScrollReveal from 'scrollreveal'
+
+const setAPIOrigin = require('../../lib/set-api-origin');
+const config = require('./config');
+const clickhandlers = require('./clickhandlers');
+const carousel = require('./carousel');
+import ScrollReveal from 'scrollreveal';
 
 $(() => {
-  setAPIOrigin(location, config)
+  setAPIOrigin(location, config);
   // scroll reveal
   window.sr = ScrollReveal({
-    origin: 'left',
-  })
-  sr.reveal('.details')
+    origin: 'left'
+  });
+  sr.reveal('.details');
 
   // add clickhandlers
   clickhandlers.addHandlers();
-  $('.menu-options').hide()
-})
+  $('.menu-options').hide();
+});
 
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 
 // use require without a reference to ensure a file is bundled
-require('./example')
+require('./example');
