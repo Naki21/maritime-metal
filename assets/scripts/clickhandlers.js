67a9f7ab80a8d45a46b048b2d3a8c777505535a2
akradin <alex.kradin@gmail.com>
2017-05-23 12:08:25 -0400

    Make mobile what we buy menu to replace Carousel
    Carousel what we buy effects mobile layout significantly
    To combat this there is a simple slideshow that shows all
    of what can be purchased which will be rendred on mobile
    views
    Made clickhandlers for this menu's buttons and added styles
    Added comments to some of the code in `index.js`

--- assets/scripts/clickhandlers.js
+++ assets/scripts/clickhandlers.js
@@ -1,14 +1,28 @@
 'use strict';
 
-function rotateMenu(){
-  console.log('click');
-  $('.menu-icon').toggleClass('open-menu');
-  // $('.menu-options').toggleClass('open-menu-options');
+let position = 0;
+function nextSlide(){
+    position -= 184;
+    let leftStyle = position;
+    $('.slide-container').css('left', leftStyle + 'px');
+    if(position <= -1840){
+      position = 0;
+    }
+}
+
+function prevSlide(){
+  $('.slide-container').css("left", "+=184px");
+  if($('.slide-container').css("left") >= '0px'){
+    $('.slide-container').css("left", "-1840px");
+  }
 }
 
 const addHandlers = () => {
-  $('.menu-icon').on('click', rotateMenu);
+  $('.slideshow-next').on('click', nextSlide);
+  $('.slideshow-previous').on('click', prevSlide);
 }
+
+
 
 module.exports = {
   addHandlers
